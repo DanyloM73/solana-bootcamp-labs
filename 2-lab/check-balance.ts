9d91e4b8efdc8aeaf861e0adf0350cc93cf83900
DanyloM73 <danyamalanin7@gmail.com>
2024-06-08 11:33:02 +0300

    Add 2 lab

---
+++ 2-lab/check-balance.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(`⚡️ Connected to devnet`);
+
+const publicKey = new PublicKey("8U7HS3BApKwDdwVAropA1uX9dJ1moPbj6N1z6gryL3r2");
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
